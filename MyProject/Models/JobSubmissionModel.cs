ecc32d23ae16798f6d248a0342a0142e39c814c2
Emily Fothergill <emily.fothergill@btopenworld.com>
2018-12-09 13:20:09 +0000

    Added attributes to Job submission page

---
+++ MyProject/Models/JobSubmissionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyProject.Models
+{
+    public class JobSubmissionModel
+    {
+        [Required, Key]
+        public string Title { get; set; }
+        public string Developer { get; set; }
+        public string Description { get; set; }
+        [MinLength(1), MaxLength(3)]
+        public string AgeRating { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public string AvailablePlatforms { get; set; }
+    }
+}
