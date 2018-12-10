a80eec4e841ab3cdc131d78954e9049bb980b4f7
Emily Fothergill <emily.fothergill@btopenworld.com>
2018-12-10 19:52:52 +0000

    Removed the home page design, trying to make a list of available jobs

--- MyProject/Controllers/HomeController.cs
+++ MyProject/Controllers/HomeController.cs
@@ -1,16 +1,45 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyProject.Data;
 using MyProject.Models;
 
 namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
+        //private ApplicationDbContext db;
+
+        //[HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        //[HttpPost]
+        //public IActionResult Index(JobSubmissionModel submittedJob)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.JobSubmissions.Add(submittedJob);
+        //        db.SaveChanges();
+
+        //        return View(submittedJob);
+        //    }
+        //    return View();
+        //}
+
+        //[HttpGet]
+        //public IActionResult JobList()
+        //{
+        //    AvailableJobViewModel availableJobViewModel = new AvailableJobViewModel();
+
+        //    availableJobViewModel.Jobs = db.JobSubmissions.ToList<JobSubmissionModel>();
+        //    availableJobViewModel.NumberOfJobs = availableJobViewModel.Jobs.Count;
+
+        //    return View(availableJobViewModel);
+        //}
 
         [Authorize]
         public IActionResult About()
---
+++ MyProject/Models/AvailableJobViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class AvailableJobViewModel
+    {
+        public int NumberOfJobs { get; set; }
+        public List<JobSubmissionModel> Jobs { get; set; }
+    }
+}
