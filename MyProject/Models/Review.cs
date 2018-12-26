3427197ed55483f37b1a9016cd328e46c1f305bc
Emily Fothergill <emily.fothergill@btopenworld.com>
2018-12-26 10:39:39 +0000

    Review Model added

---
+++ MyProject/Models/Review.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class Review
+    {
+        [Key, ScaffoldColumn(false)]
+        public int RId { get; set; }
+
+        public int GraphicQuality { get; set; }
+
+        public int Playability { get; set; }
+
+        public int StoryCharacterDevelopment { get; set; }
+
+        public int GameplayControls { get; set; }
+
+        public int Multiplayer { get; set; }
+
+        public string[] Pros { get; set; }
+
+        public string[] Cons { get; set; }
+
+        public string WrittenReview { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
