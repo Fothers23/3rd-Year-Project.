4caabbdba2684c639dce7ea0b11683995c8e74bd
Emily Fothergill <emily.fothergill@btopenworld.com>
2019-01-16 15:32:22 +0000

    Edit review method implemented but overall rating goes to 0 when edit submitted

--- MyProject/Controllers/GamesController.cs
+++ MyProject/Controllers/GamesController.cs
@@ -203,6 +203,55 @@ namespace MyProject.Controllers
             return _context.Games.Any(e => e.GameID == id);
         }
 
+        public async Task<IActionResult> EditReview(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return View(review);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditReview(int id, [Bind("ReviewID,GraphicQuality,Playability," +
+            "StoryCharacterDevelopment,GameplayControls,Multiplayer,Pros,Cons,WrittenReview," +
+            "Summary")] Review review)
+        {
+            if (id != review.ReviewID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.ReviewID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(review);
+        }
+
         public async Task<IActionResult> DeleteReview(int? id)
         {
             if (id == null)
@@ -228,6 +277,11 @@ namespace MyProject.Controllers
             _context.Reviews.Remove(review);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(e => e.ReviewID == id);
         }
     }
 }
