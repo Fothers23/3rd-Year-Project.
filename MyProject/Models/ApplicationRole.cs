b2f8feb46934344c63a95184e5dd49382f10a633
Emily Fothergill <emily.fothergill@btopenworld.com>
2019-02-20 16:07:05 +0000

    Trying to apply role-based authorization

---
+++ MyProject/Models/ApplicationRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Data;
+
+namespace MyProject.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+
+        public ApplicationRole(string roleName) : base(roleName)
+        {
+
+        }
+
+        public ApplicationRole(string roleName, string description) : base(roleName)
+        {
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+
+    }
+}
