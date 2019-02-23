64a954f275dff313f97bc9150ffe5f852f3e5820
Emily Fothergill <emily.fothergill@btopenworld.com>
2019-02-23 15:35:09 +0000

    Revert "Deleted Migrations in attempt to fix update-database issue. (REVERT)"
    This reverts commit 7982dc25875c047c1fa5001c242eb3852ab5cc74.

---
+++ MyProject/Data/Migrations/20181220142615_Games.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Data.Migrations
+{
+    public partial class Games : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobSubmissions");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 60, nullable: false),
+                    Developer = table.Column<string>(maxLength: 60, nullable: true),
+                    Description = table.Column<string>(maxLength: 150, nullable: true),
+                    AgeRating = table.Column<string>(maxLength: 3, nullable: true),
+                    Genre = table.Column<string>(maxLength: 60, nullable: true),
+                    NumberOfPlayers = table.Column<int>(nullable: false),
+                    AvailablePlatforms = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.CreateTable(
+                name: "JobSubmissions",
+                columns: table => new
+                {
+                    Title = table.Column<string>(nullable: false),
+                    AgeRating = table.Column<string>(maxLength: 3, nullable: true),
+                    AvailablePlatforms = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    Developer = table.Column<string>(maxLength: 50, nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    NumberOfPlayers = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobSubmissions", x => x.Title);
+                });
+        }
+    }
+}
---
+++ MyProject/Data/Migrations/20190117210443_Users.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Data.Migrations
+{
+    public partial class Users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Developer",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RequesterId",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyDescription",
+                table: "AspNetUsers",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeveloperName",
+                table: "AspNetUsers",
+                maxLength: 60,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_RequesterId",
+                table: "Games",
+                column: "RequesterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_AspNetUsers_RequesterId",
+                table: "Games",
+                column: "RequesterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_AspNetUsers_RequesterId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_RequesterId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "RequesterId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "DeveloperName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyDescription",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Developer",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
